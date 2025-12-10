908ac2dd410cbdcfd20e1feb0a42a41a3fed9e2a
Sophia <24-03168@g.batstate-u.edu.ph>
2025-12-10 22:10:50 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// PROJECT-LEVEL build.gradle.kts (root build script)
+// Do NOT place module dependencies here.
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
